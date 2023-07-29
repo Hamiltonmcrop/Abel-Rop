5a87e70b9583b20dab8e35f239daf287018c240f
Hamiltonmcrop <hamiltonmcrop1@gmail.com>
2023-07-29 16:32:34 +0300

    lmemman.c

--- SOSO3/lmemman.c
+++ SOSO3/lmemman.c
@@ -24,25 +24,125 @@ bool init_logical_memory(PCB *p, uint32_t code_size) {
 
 	// TODO: see background material on what this function should
 	// do. High-level objectives are:
-	// 1) calculate the number of frames necessary for program code,
-	//    user-mode stack and kernel-mode stack
-	// 2) allocate frames for above
-	// 3) determine beginning physical address of program code,
-	//    user-mode stack and kernel-mode stack
-	// 4) calculate the number of frames necessary for page directory
-	//    and page tables
-	// 5) allocate frames for above
-	// 6) set up page directory and page tables
-	// 7) set mem struct in PCB: start_code, end_code, start_stack,
-	//    start_brk, brk, and page_directory
-	// Return value: TRUE if everything goes well; FALSE if allocation
-	//     of frames failed (you should dealloc any frames that may
-	//     have already been allocated before returning)
+    uint32_t num_code_frames = bytes_to_frames(code_size);
 
-	// TODO: uncomment following line when you start working in 
-	//        this function
+    /**
+     * 12KB 3 frames
+    */
+    uint32_t num_user_frames = bytes_to_frames(1024 * 12);
 
-	return FALSE;
+    /**
+     * 4KB 1 frame
+    */
+    uint32_t kernel_stack_frames = bytes_to_frames(4096);
+
+    uint32_t cstart = (uint32_t) alloc_frames(num_code_frames, USER_ALLOC);
+    uint32_t ustart = (uint32_t) alloc_frames(num_user_frames, USER_ALLOC);
+    uint32_t kstart = (uint32_t) alloc_frames(kernel_stack_frames, KERNEL_ALLOC);
+    
+    /**
+     * 4KB page directory
+    */
+    uint32_t dframes = bytes_to_frames(4096);//for page directory
+    uint32_t pframes = bytes_to_frames(4096 * 2);//for page table
+    
+    uint32_t dfstart = (uint32_t) alloc_frames(dframes, KERNEL_ALLOC);
+    /**
+     * for the page table
+    */
+    uint32_t ptstart = (uint32_t) alloc_frames(pframes, KERNEL_ALLOC);
+
+    PTE* ptentry = (PTE*) (ptstart + KERNEL_BASE);
+
+    /**
+     * declare an iterator
+    */
+    int i;
+
+    for(i = 0; i < 1024; i++)
+    {
+        if(i == 0)
+        {
+            ptentry[i] = cstart | PTE_PRESENT | PTE_READ_WRITE | PTE_USER_SUPERVISOR;
+        }
+        else ptentry[i] = 0;
+    }
+
+    /**
+     * 2nd PTE
+    */
+    for(i = 0; i < 1024; i++)
+    {
+        if(i == 1023)
+        {
+            ptentry[i + 1024] = kstart | PTE_PRESENT | PTE_READ_WRITE;
+        }
+
+        else if((i < 1023) && (i > 1019))
+        {
+            ptentry[i + 1024] = ustart + ((i - 1020) * 4096) | PTE_PRESENT | PTE_USER_SUPERVISOR;
+        }
+
+        else ptentry[i + 1024] = 0;
+    }
+
+    /**
+     * handle page directory table
+    */
+    PDE* pdtable = (PDE*) (dfstart + KERNEL_BASE);
+    uint32_t paddress = (uint32_t) dfstart;
+    for(i = 0; i < 1024; i++)
+    {
+        if(i == 0)
+        {
+            paddress = (uint32_t) pframes;
+            pdtable[i] = paddress | PDE_PRESENT | PDE_READ_WRITE | PDE_USER_SUPERVISOR;
+        }
+        else if(i == 766) //stack starts here
+        {
+            paddress = (uint32_t) ptstart + 4096;
+        }
+
+        else if(i == 768) //2nd case + 2
+        {
+            pdtable[i] = k_page_directory[768];
+        }
+
+        else pdtable[i] = 0;
+    }
+
+    p->mem.start_code = 0;
+    p->mem.end_code = code_size - 1;
+    p->mem.start_brk = num_code_frames * 4096;
+    p->mem.start_stack = 0xBFBFEFFF;
+    p->mem.page_directory = (PDE*) dfstart;
+
+    if(cstart == NULL || ustart == NULL || kstart == NULL || dfstart == NULL || ptstart == NULL)
+    {
+        if(cstart != NULL)
+        {
+            dealloc_frames ((void *) cstart + KERNEL_BASE, num_code_frames);
+        }
+        
+        if(ustart != NULL)
+        {
+            dealloc_frames ((void *) ustart + KERNEL_BASE, num_user_frames);
+        }
+        
+        if(kstart != NULL)
+        {
+            dealloc_frames ((void *) kstart + KERNEL_BASE, kernel_stack_frames);
+        }
+
+        if(ptstart != NULL)
+        {
+            dealloc_frames ((void *) ptstart + KERNEL_BASE, pframes);
+        }
+
+        return FALSE;
+    }
+
+	return TRUE;
 }
 
 /*** Initialize kernel's page directory and table ***/
@@ -149,5 +249,3 @@ void zero_out_pages(void *base, uint32_t n_pages) {
 	for (i=0; i<1024*n_pages; i++)
 		*((uint32_t *)((uint32_t)base + i)) = 0;
 }
-
-
